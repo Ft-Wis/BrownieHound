ae923e49449d70da9328f55c82dc43f8d440e426
藤原拓海 <94789654+Ft-Wis@users.noreply.github.com>
2023-04-24 14:19:53 +0900

    Add files via upload

---
+++ BrownieHound/BrownieHound/packet_detail.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrownieHound
+{
+    /// <summary>
+    /// packet_detail.xaml の相互作用ロジック
+    /// </summary>
+    public partial class packet_detail : Page
+    {
+        public packet_detail()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BrownieHound/BrownieHound/rule_add.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrownieHound
+{
+    /// <summary>
+    /// rule_add.xaml の相互作用ロジック
+    /// </summary>
+    public partial class rule_add : Page
+    {
+        public rule_add()
+        {
+            InitializeComponent();
+        }
+    }
+}
