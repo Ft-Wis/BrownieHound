ae8c4516e7aec9952a61fae5c7817de6c706e676
Ft-Wis <fyrwy945@gmail.com>
2023-05-02 10:48:34 +0900

    tsharkから情報を取得

--- BrownieHound/BrownieHound/capture.xaml.cs
+++ BrownieHound/BrownieHound/capture.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +11,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace BrownieHound
 {
@@ -18,15 +25,130 @@ namespace BrownieHound
     /// </summary>
     public partial class capture : Page
     {
+        public class packetData
+        {
+            public string Data { get; set; }
+            public packetData(string msg)
+            {
+                Data = msg;
+            }
+        }
+        Process processTsinterface = null;
+        Process processTscap = null;
+        private ObservableCollection<packetData> CData;
         public capture()
         {
             InitializeComponent();
+            CaputureData.ItemsSource = CData;
+            CData = new ObservableCollection<packetData> { };
         }
-
+        
         private void inactivate_Click(object sender, RoutedEventArgs e)
         {
+            if(processTsinterface != null && !processTsinterface.HasExited)
+            {
+                processTsinterface.Kill();
+            }
+            if (processTscap != null && !processTscap.HasExited)
+            {
+                processTscap.Kill();
+            }
             Application.Current.Shutdown();
 
+        }
+
+        private void Page_loaded(object sender, RoutedEventArgs e)
+        {
+            string Command = "C:\\Program Files\\Wireshark\\tshark.exe";
+
+            string args = "-i 5";
+            //オプションとしてテスト用に固定値を指定
+            processTscap = new Process();
+            ProcessStartInfo processSinfo = new ProcessStartInfo(Command, args);
+            processSinfo.CreateNoWindow = true;
+            processSinfo.UseShellExecute = false;
+            processSinfo.RedirectStandardOutput = true;
+            processSinfo.RedirectStandardError = true;
+
+            processSinfo.StandardErrorEncoding = Encoding.UTF8;
+            processSinfo.StandardOutputEncoding = Encoding.UTF8;
+
+            processTscap = Process.Start(processSinfo);
+
+            processTscap.OutputDataReceived += dataReceived;
+            processTscap.ErrorDataReceived += errReceived;
+
+            processTscap.BeginErrorReadLine();
+            processTscap.BeginOutputReadLine();
+            /*
+            string Command = "C:\\Program Files\\Wireshark\\tshark.exe";
+
+            string args = "-D";
+
+            processTsinterface = new Process();
+            ProcessStartInfo processSinfo = new ProcessStartInfo(Command,args);
+            processSinfo.CreateNoWindow = true;
+            processSinfo.UseShellExecute = false;
+            processSinfo.RedirectStandardOutput = true;
+            processSinfo.RedirectStandardError = true;
+
+            processSinfo.StandardErrorEncoding = Encoding.UTF8;
+            processSinfo.StandardOutputEncoding = Encoding.UTF8;
+
+            processTsinterface = Process.Start(processSinfo);
+
+            processTsinterface.OutputDataReceived += dataReceivedTsinterface;
+            processTsinterface.ErrorDataReceived += errReceivedTsinterface;
+
+            processTsinterface.BeginErrorReadLine();
+            processTsinterface.BeginOutputReadLine();
+            */
+        }
+
+        private void errReceived(object sender, DataReceivedEventArgs e)
+        {
+            string packetText = e.Data;
+            if (packetText != null && packetText.Length > 0)
+            {
+                PrintTextBoxByThread("ERR:" + packetText);
+            }
+        }
+
+        private void dataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string packetText = e.Data;
+            if (packetText != null && packetText.Length > 0)
+            {
+                PrintTextBoxByThread(packetText);
+            }
+        }
+        /*
+        void dataReceivedTsinterface(object sender, DataReceivedEventArgs e)
+        {
+            string packetText = e.Data;
+            if(packetText != null && packetText.Length > 0)
+            {
+                PrintTextBoxByThread(packetText, captureTest);
+            }
+        }
+        void errReceivedTsinterface(object sender,DataReceivedEventArgs e)
+        {
+            string packetText = e.Data;
+            if (packetText != null && packetText.Length > 0)
+            {
+                PrintTextBoxByThread("ERR:" + packetText, captureTest);
+            }
+        }
+        */
+        private void PrintText(string msg)
+        {
+            CData.Add(new packetData(msg));
+            CaputureData.ItemsSource = CData;
+            CaputureData.ScrollIntoView(CaputureData.Items.GetItemAt(CaputureData.Items.Count - 1));
+        }
+        private void PrintTextBoxByThread(string msg)
+        {
+            Dispatcher.Invoke(new Action(() => PrintText(msg)));
         }
     }
 }
