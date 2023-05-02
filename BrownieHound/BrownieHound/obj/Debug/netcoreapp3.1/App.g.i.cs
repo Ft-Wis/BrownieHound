82b6ab781b21b9256b383c68b1b924e31f808bed
Ft-Wis <fyrwy945@gmail.com>
2023-05-02 14:45:36 +0900

    テスト

--- BrownieHound/BrownieHound/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ BrownieHound/BrownieHound/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -47,7 +47,7 @@ namespace BrownieHound {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace BrownieHound {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/BrownieHound;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BrownieHound;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace BrownieHound {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public static void Main() {
             BrownieHound.App app = new BrownieHound.App();
             app.InitializeComponent();
