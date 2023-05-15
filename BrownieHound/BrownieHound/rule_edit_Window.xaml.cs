056162bb41ce701afc6ee7f1e5acbf5a7668effe
Nakane Takumi <ctb20.nakane.takumi@gmail.com>
2023-05-15 14:47:08 +0900

    画面遷移の実装(解析実行からは除く)の実装

---
+++ BrownieHound/BrownieHound/rule_edit_Window.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BrownieHound
+{
+    /// <summary>
+    /// rule_edit_Window.xaml の相互作用ロジック
+    /// </summary>
+    public partial class rule_edit_Window : Window
+    {
+        public rule_edit_Window()
+        {
+            InitializeComponent();
+
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void editButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
