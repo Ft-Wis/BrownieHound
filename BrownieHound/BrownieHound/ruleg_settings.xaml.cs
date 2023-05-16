336d4e2110c02d42c8911c45890960a72ecc01d5
Nakane Takumi <ctb20.nakane.takumi@gmail.com>
2023-05-16 14:18:24 +0900

    borderの配置

--- BrownieHound/BrownieHound/ruleg_settings.xaml.cs
+++ BrownieHound/BrownieHound/ruleg_settings.xaml.cs
@@ -25,14 +25,18 @@ namespace BrownieHound
 
         private void addButton_Click(object sender, RoutedEventArgs e)
         {
-            var nextPage = new rule_edit();
-            NavigationService.Navigate(nextPage);
+            rule_add_Window ruleAddWin = new rule_add_Window();
+            ruleAddWin.ShowDialog();
+            //var nextPage = new rule_edit();
+            //NavigationService.Navigate(nextPage);
         }
 
         private void detailButton_Click(object sender, RoutedEventArgs e)
         {
-            var nextPage = new ruleg_detail();
-            NavigationService.Navigate(nextPage);
+            rule_edit_Window ruleEditWin = new rule_edit_Window();
+            ruleEditWin.ShowDialog();
+            //var nextPage = new ruleg_detail();
+            //NavigationService.Navigate(nextPage);
         }
 
         private void inactivate_Click(object sender, RoutedEventArgs e)
