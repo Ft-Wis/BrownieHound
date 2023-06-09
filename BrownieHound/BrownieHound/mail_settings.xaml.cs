0e54f59a4dac28592f6e6e228d2d5cd964c2f94d
sotsuken2024 <nkc2024sotsuken@outlook.jp>
2023-06-09 12:38:22 +0900

    メール送信はできた。 学校のWi-Fiだとブロックされて送信できないので、自分のスマホからテザリングしてやる必要アリ

--- BrownieHound/BrownieHound/mail_settings.xaml.cs
+++ BrownieHound/BrownieHound/mail_settings.xaml.cs
@@ -10,6 +10,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using System.Net;
+//using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
 
 namespace BrownieHound
 {
@@ -21,7 +27,8 @@ namespace BrownieHound
         public mail_settings()
         {
             InitializeComponent();
-
+            SendEmailNew();
+            //SendEmail();   
         }
 
         private void s_rTotop_redo_Click(object sender, RoutedEventArgs e)
@@ -39,6 +46,45 @@ namespace BrownieHound
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void SendEmail()
+        {
+            //SmtpClient client = new SmtpClient("smtp.gmail.com");
+            //client.Port = 587;
+            //client.EnableSsl = true;
+            //client.UseDefaultCredentials = false;
+            //client.Credentials = new NetworkCredential("browniehound2024@gmail.com", "Sotsuken2024");
+
+            //MailAddress from = new MailAddress("browniehound2024@gmail.com");
+            //MailAddress to = new MailAddress("ctb20.nakane.takumi@gmail.com");
+
+            //MailMessage message = new MailMessage(from, to);
+            //message.Subject = "テスト送信";
+            //message.Body = "browniehoundですメールアドレスの認証を行ってください";
+            //client.Send(message);
+        }
+
+        private void SendEmailNew()
+        {
+            var email = new MimeMessage();
+
+            email.From.Add(new MailboxAddress("browniehound", "browniehound2024@gmail.com"));
+            email.To.Add(new MailboxAddress("you", "ctb20.nakane.takumi@gmail.com"));
+
+            email.Subject = "テスト送信";
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = "MailKit を使ってメールを送ってみるテストです。"
+            };
+
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Connect("smtp.gmail.com",587,false);
+                smtp.Authenticate("browniehound2024", "eszyyyyhrwarlsns");
+                smtp.Send(email);
+                smtp.Disconnect(true);
+            }
         }
 
         private void ToggleButton_Checked(object sender, RoutedEventArgs e)
