1bc424961a049b1b447120f89b795b497f8dd71c
sotsuken2024 <nkc2024sotsuken@outlook.jp>
2023-10-17 14:10:19 +0900

    RuleData.csの作成

---
+++ BrownieHound/BrownieHound/RuleData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrownieHound
+{
+    public class RuleData
+    {
+        public class ruleData
+        {
+            public int ruleGroupNo { get; set; }
+            public int ruleNo { get; set; }
+            public int detectionInterval { get; set; }
+            public int detectionCount { get; set; }
+            public string Source { get; set; }
+            public string Destination { get; set; }
+            public string Protocol { get; set; }
+            public string sourcePort { get; set; }
+            public string destinationPort { get; set; }
+            public int frameLength { get; set; }
+            public int ruleCategory { get; set; } = 0;
+
+            private void ruleSplit(string ruleSeet)
+            {
+                string[] data = ruleSeet.Split(',');
+                int i = 0;
+                ruleCategory = Int32.Parse(data[i++]);
+                detectionInterval = Int32.Parse(data[i++]);
+                detectionCount = Int32.Parse(data[i++]);
+                Source = data[i++];
+                Destination = data[i++];
+                Protocol = data[i++];
+                sourcePort = data[i++];
+                destinationPort = data[i++];
+                frameLength = Int32.Parse(data[i]);
+            }
+            public ruleData(string ruleSeet, int ruleGroupNo, int ruleNo)
+            {
+                this.ruleGroupNo = ruleGroupNo;
+                this.ruleNo = ruleNo;
+                ruleSplit(ruleSeet);
+            }
+
+            public ruleData(string ruleSheet)
+            {
+                ruleSplit(ruleSheet);
+            }
+        }
+    }
+}
