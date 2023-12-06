04a3e7d89f653ee4cdcc6414ce57a7b74eccc07d
sotsuken2024 <nkc2024sotsuken@outlook.jp>
2023-12-06 09:56:34 +0900

    フォルダ選択ダイアログを実装

---
+++ BrownieHound/BrownieHound/detectionPacketsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrownieHound
+{
+    /// <summary>
+    /// detectionPacketsView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class detectionPacketsView : Page
+    {
+        public detectionPacketsView(string pathToFolder)
+        {
+            InitializeComponent();
+            System.Windows.MessageBox.Show($"{pathToFolder}を選択しました");
+        }
+    }
+}
